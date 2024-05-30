1c4a1ec07ba94b57a53161920030a3b761de5151
muhammed-kutty <mkkunnathodi@gmail.com>
2024-05-30 17:04:39 +0530

    create pages and aroute

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import SignIn from './Pages/SignIn'
+import SignUp from './Pages/SignUp'
+import About from './Pages/About'
+import Profile from './Pages/Profile'
 
 const App = () => {
   return (
-    <h1 className='text-fuchsia-800'>
-      Helo
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/signin' element={<SignIn/>} />
+        <Route path='/signup' element={<SignUp/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/profile' element={<Profile/>} />
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 

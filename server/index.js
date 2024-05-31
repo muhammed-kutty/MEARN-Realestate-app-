c286c14d74f326b38211b2991ac8e9e6a64b8671
muhammed-kutty <mkkunnathodi@gmail.com>
2024-05-31 09:07:50 +0530

    create server and run It

---
+++ server/index.js
@@ -0,0 +1,14 @@
+import express from "express"
+import dotenv from "dotenv"
+
+
+dotenv.config()
+const app = express()
+const PORT = process.env.PORT
+
+
+
+
+app.listen(PORT , ()=>{
+    console.log(`sever Running at localhont${PORT}`);
+}) 
